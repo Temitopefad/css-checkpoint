f0cec64b99ac0a6b0f05653a132a017d38bbd305
Temitopefad <greycefad9@gmail.com>
2024-08-27 09:40:15 +0100

    css commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//let firstname = 'temitope'
+//console.log(firstname)
+
+//function showname(){
+   // console.log("Temitope")
+//}
+//showname()
+
+
+//let numbers= [56,89,34,12,45] // replace 12 with 100
+//console.log(number[3])
+//numbers[3] = 100
+//console.log(numbers)
+
+
+
+//const person ={
+  // firstname:"arya" ,
+  // lastname: "mike" ,
+  //email: "one@gmail.com" ,
+
+//}
+
+// write a funtion that takes two parametres and multiply them
+
+ function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply (5,5))
+
